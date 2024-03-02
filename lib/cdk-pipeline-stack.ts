f0148c58aa461fdf4ccd47d926974c9a68a4ed83
Arjan Hoek <arjanhoek5191@gmail.com>
2024-03-02 20:51:51 +0100

    add pre-prod stage

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -1,6 +1,11 @@
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-import { Construct } from 'constructs';
-import {  Stack, StackProps } from 'aws-cdk-lib';
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+import { Stack, StackProps } from "aws-cdk-lib";
+import { CdkEBStage } from "./eb-stage";
 
 /**
  * The stack that defines the application pipeline
@@ -9,25 +14,35 @@ export class CdkPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, "Pipeline", {
       // The pipeline name
-      pipelineName: 'MyServicePipeline',
+      pipelineName: "MyServicePipeline",
 
-       // How it will be built and synthesized
-       synth: new ShellStep('Synth', {
-         // Where the source can be found
-         input: CodePipelineSource.gitHub('ArjanHoek/ElasticBeanstalkApp', 'main'),
-         
-         // Install dependencies, build and run cdk synth
-         installCommands: ['npm i -g npm@latest'],
-         commands: [
-           'npm ci',
-           'npm run build',
-           'npx cdk synth'
-         ],
-       }),
+      // How it will be built and synthesized
+      synth: new ShellStep("Synth", {
+        // Where the source can be found
+        input: CodePipelineSource.gitHub(
+          "ArjanHoek/ElasticBeanstalkApp",
+          "main"
+        ),
+
+        // Install dependencies, build and run cdk synth
+        installCommands: ["npm i -g npm@latest"],
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
     });
 
     // This is where we add the application stages
+
+    // deploy beanstalk app
+    // For environment with all default values:
+    // const deploy = new CdkEBStage(this, 'Pre-Prod');
+
+    // For environment with custom AutoScaling group configuration
+    const deploy = new CdkEBStage(this, "Pre-Prod", {
+      minSize: "1",
+      maxSize: "2",
+    });
+    const deployStage = pipeline.addStage(deploy);
   }
 }
